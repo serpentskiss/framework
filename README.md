20273ddc3e51a2d1e82525141132724025c793a1
jonthompson <jonthompson@192.168.2.114>
2017-10-02 16:39:45 +0100

    README

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A custom lightweight PHP framework
 * Download and extract the zip file
 * Copy the files to the root of your project
 
+The folder named **web** here is the web root of your webspace. 
+
+In your web browser, navigate to your site, and run the installer
---
+++ web/install.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * INSTALLER
+ * 
+ * @name        install.php
+ * @package     git-framework.local
+ * @version     1.0.0
+ * @since       02-Oct-2017 16:37:07
+ * @author      Jon Thompson
+ * @abstract    Script to install the necessary files/folders to the framework
+ */
+
+
+
+
+?>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta  name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'>
+        <link rel="icon" type="image/png" href="http://remote.jonthompson.co.uk/generic/images/gearIcon.png" sizes="32x32">
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <meta name="description" content="Framework installer">
+        <meta name="keywords" content="">
+        <meta name="author" content="Jon Thompson">
+
+        <title>:: INSTALLER ::</title>
+
+        
+        <script src="/js/jquery.js"></script>
+        <script src="/js/jquery.cookie.js"></script>
+        <script src="/js/prism.js"></script>
+        <script src="/js/jquery.form.js"></script>
+        <script src="/js/client.min.js"></script>
+        
+        <link rel="stylesheet" href="/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/css/grids-2.css">
+        <link rel="stylesheet" href="/css/prism.css">
+        <link rel="stylesheet" href="/css/terminal.css">
+        <link rel="stylesheet" href="/css/responseMessages.css">
+    </head>
+    <body>
+        <main id='main'>
+            
+        </main>
+    </body>
+</html>
