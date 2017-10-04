b8fd1231e2ceeed90758e3117e2ae71c8bf19a45
jonthompson <jonthompson@192.168.2.114>
2017-10-04 16:29:07 +0100

    Added custom error trapping and some default basic classes

--- web/templates/default/header.php
+++ web/templates/default/header.php
@@ -16,22 +16,24 @@
     <head>
         <meta charset='utf-8'>
         <meta  name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'>
-        <link rel="icon" type="image/png" href="http://remote.jonthompson.co.uk/generic/images/gearIcon.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="" sizes="32x32">
         <!--[if lt IE 9]>
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
 
-        <meta name="description" content="Full-height, single-column responsive template with fixed header and footer">
-        <meta name="keywords" content="full-height, single-column, responsive, html template, fixed header, fixed footer">
-        <meta name="author" content="Jon Thompson">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
 
         <title><?php echo \config\config::SITE_NAME; ?></title>
 
         
-        <script src="/js/jquery.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
+        
         
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
-        <link rel="stylesheet" href="/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
         <link rel="stylesheet" href="/css/php.css?<?php echo time(); ?>">
         
     </head>
