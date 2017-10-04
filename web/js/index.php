73db525b96898bb8d2439a1a1d75bb8e6f0d2ca2
jonthompson <jonthompson@192.168.2.114>
2017-10-04 09:34:24 +0100

    Added blank index pages to web-accessible folders to cover hosts where directory browsing is on

---
+++ web/js/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * BLANK INDEX PAGE
+ * 
+ * @name        index.php
+ * @package     git-framework.local
+ * @version     
+ * @since       04-Oct-2017 09:33:06
+ * @author      Jon Thompson
+ * @abstract    
+ */
+
+/**
+ * Silence is golden
+ */
